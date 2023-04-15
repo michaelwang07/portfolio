12185de381be397701ce8da5eeb71fbb864b8d96
michaelwang07 <michaelwang07@yahoo.com>
2023-04-15 00:57:44 -0700

    more routing fixes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -15,16 +15,16 @@ const Navbar = () => {
       </div>
       <ul className='app__navbar-links'>
         <li>
-          <a href="/" className='content-2'>Home</a>
+          <a href="/portfolio/" className='content-2'>Home</a>
         </li>
         <li>
-          <a href="/about" className='content-2'>About</a>
+          <a href="/portfolio/about" className='content-2'>About</a>
         </li>
         <li>
-          <a href="/resume" className='content-2'>Resume</a>
+          <a href="/portfolio/resume" className='content-2'>Resume</a>
         </li>
         <li>
-          <a href="/contact" className='content-2'>Contact</a>
+          <a href="/portfolio/contact" className='content-2'>Contact</a>
         </li>
         <li>
           <div className='app__navbar-socialmedia'>
@@ -55,16 +55,16 @@ const Navbar = () => {
         {toggleMenu && (
         <ul className='app__navbar-smallscreen_links'>
           <li>
-            <a href="/" className='content-2'>Home</a>
+            <a href="/portfolio/" className='content-2'>Home</a>
           </li>
           <li>
-            <a href="/about" className='content-2'>About</a>
+            <a href="/portfolio/about" className='content-2'>About</a>
           </li>
           <li>
-            <a href="/resume" className='content-2'>Resume</a>
+            <a href="/portfolio/resume" className='content-2'>Resume</a>
           </li>
           <li>
-            <a href="/contact" className='content-2'>Contact</a>
+            <a href="/portfolio/contact" className='content-2'>Contact</a>
           </li>
         </ul>
         )}
