72d79b9825c2242782a600531fd5f709738d9688
michaelwang07 <michaelwang07@yahoo.com>
2023-04-17 23:51:07 -0700

    email form contact

--- src/components/contact_me/contact_me.jsx
+++ src/components/contact_me/contact_me.jsx
@@ -22,6 +22,9 @@ const contact_me = () => {
       <div className='app__contact-description'>
         <p className='paragraph-1'>Are you looking for a talented and motivated Junior Frontend Developer to join your team? Look no further! As a passionate frontend developer with a solid foundation in HTML, CSS, and JavaScript, I am eager to contribute my skills to your organization. I have hands-on experience in developing user-friendly and responsive web applications, with a keen eye for detail and a commitment to writing clean and maintainable code. I am well-versed in modern frontend frameworks such as React and Vue, and have a strong understanding of UI/UX principles. With a strong work ethic, excellent communication skills, and a willingness to learn and grow, I am confident that I would be a valuable addition to your team. Contact me today to discuss how I can contribute to your organization's success as a Junior Frontend Developer!</p>
       </div>
+      <div className='app__contact-emailme'>
+        <h1>Email Me</h1>
+      </div>
     </div>
   )
 }
